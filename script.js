99ee7ac6556b0567d5a2cf0fb16baf162f1e194f
vladlenn13 <146069086+vladlenn13@users.noreply.github.com>
2023-12-05 01:49:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,4 @@
-function handleOrderSelection(elementId, orderElementId) {
-  var menuItem = document.getElementById(elementId);
-  var orderItem = document.getElementById(orderElementId);
 
-  menuItem.addEventListener('click', function(e) {
-    e.preventDefault();
-    orderItem.classList.toggle('checked');
-    updateOrderList(orderElementId, orderItem.classList.contains('checked'));
-  });
-}
-
-function updateOrderList(orderElementId, isChecked) {
-  var orderList = document.getElementById('orderList');
-  var orderText = document.getElementById(orderElementId).previousSibling.textContent;
-  var existingOrderItem = document.querySelector(`#${orderElementId}-item`);
-
-  if (isChecked && !existingOrderItem) {
-    var li = document.createElement('li');
-    li.textContent = orderText;
-    li.id = `${orderElementId}-item`;
-    orderList.appendChild(li);
-  } else if (!isChecked && existingOrderItem) {
-    existingOrderItem.remove();
-  }
-}
 // Определяем обработчик для открытия/закрытия подменю
 function handleSubMenu(submenuElement) {
   submenuElement.addEventListener('click', function(e) {
