453c058296b676cdcb29387888de248016fa0502
vladlenn13 <146069086+vladlenn13@users.noreply.github.com>
2023-12-05 08:34:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var firstCourse = document.getElementById('firstCourse');
-    var secondCourse = document.getElementById('secondCourse');
-    var drink = document.getElementById('drink');
-    var salad = document.getElementById('salad');
-    var complexMealBtn = document.getElementById('complexMealBtn');
-    var complexMealSubMenu = document.getElementById('complexMealSubMenu');
-    var confirmComplexMealBtn = document.createElement('button');
-
-    confirmComplexMealBtn.textContent = 'Подтвердить';
-    complexMealSubMenu.appendChild(confirmComplexMealBtn);
-
     function updateOrderList(orderElementId, isChecked, orderText) {
         var orderList = document.getElementById('cartItems');
         var existingOrderItem = document.getElementById(`${orderElementId}-item`);
@@ -19,27 +8,33 @@ document.addEventListener('DOMContentLoaded', function() {
             li.textContent = orderText;
             li.id = `${orderElementId}-item`;
 
+            var deleteIcon = document.createElement('span');
+            deleteIcon.textContent = '❌';
+            deleteIcon.classList.add('deleteItem');
+            deleteIcon.addEventListener('click', function() {
+                li.remove();
+            });
+
+            li.appendChild(deleteIcon);
             orderList.appendChild(li);
         } else if (!isChecked && existingOrderItem) {
             existingOrderItem.remove();
         }
     }
 
-    complexMealBtn.addEventListener('click', function(e) {
-        e.preventDefault();
-        complexMealSubMenu.style.display = 'block';
-        confirmComplexMealBtn.style.display = 'block';
-    });
-
-    confirmComplexMealBtn.addEventListener('click', function(e) {
-        e.preventDefault();
-        var orderText = `Комплексный обед: ${firstCourse.value}, ${secondCourse.value}, ${drink.value}, ${salad.value}`;
-        updateOrderList('complexMeal', true, orderText);
-        complexMealSubMenu.style.display = 'none';
-        confirmComplexMealBtn.style.display = 'none';
-        firstCourse.selectedIndex = 0;
-        secondCourse.selectedIndex = 0;
-        drink.selectedIndex = 0;
-        salad.selectedIndex = 0;
+    var menuItems = document.querySelectorAll('.menu li a');
+    menuItems.forEach(function(item) {
+        if (item.nextElementSibling && item.nextElementSibling.tagName === 'UL') {
+            item.addEventListener('click', function(e) {
+                e.preventDefault();
+                var submenu = this.nextElementSibling;
+                submenu.style.display = submenu.style.display === 'none' || submenu.style.display === '' ? 'block' : 'none';
+            });
+        } else {
+            item.addEventListener('click', function() {
+                this.classList.toggle('checked');
+                updateOrderList(this.id, this.classList.contains('checked'), this.textContent);
+            });
+        }
     });
 });
