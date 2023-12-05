fac5bc609c052422fbd2b35044bcdaa8b3dcea74
vladlenn13 <146069086+vladlenn13@users.noreply.github.com>
2023-12-05 19:04:04 +0300

    Update order.js

--- order.js
+++ order.js
@@ -2,48 +2,51 @@ document.addEventListener('DOMContentLoaded', function () {
     const cellContainer = document.getElementById('cellContainer');
 
     for (let i = 1; i <= 5; i++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.dataset.id = i;
+        const cellWrapper = document.createElement('div');
+        cellWrapper.classList.add('cell');
 
-        // Создание QR-кода для каждой ячейки
+        const cellNumber = document.createElement('p');
+        cellNumber.textContent = `Ячейка №${i}`;
+        cellNumber.style.marginBottom = '5px';
+
+        const qrContainer = document.createElement('div');
+        qrContainer.classList.add('qr-container');
+
         const qr = new QRious({
-            value: `https://vladlenn13.github.io/Web/order/${i}`, // Замените ссылку на вашу страницу со статусом ячейки
-            size: 100 // Размер QR-кода
+            value: `https://vladlenn13.github.io/Web/order/${i}`,
+            size: 100
         });
 
         const qrImage = document.createElement('img');
         qrImage.src = qr.toDataURL();
         qrImage.alt = `QR-код для ячейки ${i}`;
 
-        cell.appendChild(qrImage);
-        cellContainer.appendChild(cell);
-    }
+        qrContainer.appendChild(qrImage);
+        cellWrapper.appendChild(cellNumber);
+        cellWrapper.appendChild(qrContainer);
+        cellContainer.appendChild(cellWrapper);
 
-    const cells = document.querySelectorAll('.cell');
-
-    cells.forEach(cell => {
-        cell.addEventListener('click', () => {
-            const isBooked = cell.classList.contains('booked');
-            const isReserved = cell.classList.contains('reserved');
+        cellWrapper.addEventListener('click', () => {
+            const isBooked = cellWrapper.classList.contains('booked');
+            const isReserved = cellWrapper.classList.contains('reserved');
 
             if (isBooked) {
                 alert('Ячейка занята, выберете другую!');
             } else if (isReserved) {
                 const confirmCancel = confirm('Вы хотите отменить бронь на эту ячейку?');
                 if (confirmCancel) {
-                    cell.classList.remove('reserved');
-                    cell.classList.add('booked');
+                    cellWrapper.classList.remove('reserved');
+                    cellWrapper.classList.add('booked');
                     alert('Вы успешно отменили бронь на ячейке');
                 }
             } else {
-                const confirmBooking = confirm(`Вы хотите забронировать ячейку ${cell.dataset.id}?`);
+                const confirmBooking = confirm(`Вы хотите забронировать ячейку ${i}?`);
                 if (confirmBooking) {
-                    cell.classList.remove('cell');
-                    cell.classList.add('reserved');
-                    alert(`Вы успешно забронировали ячейку ${cell.dataset.id}`);
+                    cellWrapper.classList.remove('cell');
+                    cellWrapper.classList.add('reserved');
+                    alert(`Вы успешно забронировали ячейку ${i}`);
                 }
             }
         });
-    });
+    }
 });
