f548a9b18f1c90f768597a9ea57d58161fae485f
vladlenn13 <146069086+vladlenn13@users.noreply.github.com>
2023-12-05 08:39:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var firstCourse = document.getElementById('firstCourse');
-    var secondCourse = document.getElementById('secondCourse');
-    var drink = document.getElementById('drink');
-    var salad = document.getElementById('salad');
-    var complexMealBtn = document.getElementById('complexMealBtn');
-    var complexMealSubMenu = document.getElementById('complexMealSubMenu');
-    var confirmComplexMealBtn = document.getElementById('confirmComplexMealBtn');
     var checkoutBtn = document.getElementById('checkoutBtn');
     var cartItems = document.getElementById('cartItems');
 
-    function updateOrderList(orderElementId, orderText) {
+    function updateOrderList(orderElementId, orderText, isChecked) {
         var existingOrderItem = document.getElementById(`${orderElementId}-item`);
 
-        if (!existingOrderItem) {
+        if (isChecked) {
             var li = document.createElement('li');
             li.textContent = orderText;
             li.id = `${orderElementId}-item`;
@@ -26,33 +19,54 @@ document.addEventListener('DOMContentLoaded', function() {
 
             li.appendChild(deleteIcon);
             cartItems.appendChild(li);
+        } else if (!isChecked && existingOrderItem) {
+            existingOrderItem.remove();
         }
     }
 
-    complexMealBtn.addEventListener('click', function(e) {
-        e.preventDefault();
-        complexMealSubMenu.style.display = 'block';
-        confirmComplexMealBtn.style.display = 'block';
+    function addToCart(item, itemName) {
+        item.addEventListener('click', function() {
+            this.classList.toggle('checked');
+            updateOrderList(item.id, itemName, this.classList.contains('checked'));
+        });
+    }
+
+    var menuItems = document.querySelectorAll('.menu li a');
+    menuItems.forEach(function(item) {
+        if (item.nextElementSibling && item.nextElementSibling.tagName === 'UL') {
+            item.addEventListener('click', function(e) {
+                e.preventDefault();
+                var submenu = this.nextElementSibling;
+                submenu.style.display = submenu.style.display === 'none' || submenu.style.display === '' ? 'block' : 'none';
+            });
+        } else {
+            addToCart(item, item.textContent);
+        }
     });
 
+    var confirmComplexMealBtn = document.getElementById('confirmComplexMealBtn');
+    var complexMealItems = document.querySelectorAll('#complexMealSubMenu select');
+    
     confirmComplexMealBtn.addEventListener('click', function(e) {
         e.preventDefault();
-        var orderText = `Комплексный обед: ${firstCourse.value}, ${secondCourse.value}, ${drink.value}, ${salad.value}`;
-        updateOrderList('complexMeal', orderText);
-        complexMealSubMenu.style.display = 'none';
-        confirmComplexMealBtn.style.display = 'none';
-        resetSelects();
+        var orderText = 'Комплексный обед: ';
+        complexMealItems.forEach(function(item, index) {
+            orderText += item.value;
+            if (index !== complexMealItems.length - 1) {
+                orderText += ', ';
+            }
+        });
+        updateOrderList('complexMeal', orderText, true);
+        resetComplexMeal();
     });
 
-    function resetSelects() {
-        firstCourse.selectedIndex = 0;
-        secondCourse.selectedIndex = 0;
-        drink.selectedIndex = 0;
-        salad.selectedIndex = 0;
+    function resetComplexMeal() {
+        complexMealItems.forEach(function(item) {
+            item.selectedIndex = 0;
+        });
     }
 
     checkoutBtn.addEventListener('click', function() {
-        // Дополнительная логика для оформления заказа, если необходимо
-        // Здесь вы можете добавить обработку оформления заказа
+        // Дополнительная логика для оформления заказа
     });
 });
