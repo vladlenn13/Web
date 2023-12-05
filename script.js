d811911096fb0f6c7ee7e85fc4a7ff52d8042dc6
vladlenn13 <146069086+vladlenn13@users.noreply.github.com>
2023-12-05 07:46:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,29 +2,48 @@ function addToCart(item) {
     const cartItems = document.getElementById('cartItems');
     const listItem = document.createElement('li');
     listItem.textContent = item;
+
+    // Добавление крестика для удаления из корзины
+    const deleteIcon = document.createElement('span');
+    deleteIcon.textContent = '❌';
+    deleteIcon.classList.add('deleteItem');
+    deleteIcon.addEventListener('click', function () {
+        listItem.remove();
+    });
+
+    listItem.appendChild(deleteIcon);
     cartItems.appendChild(listItem);
 }
 
-function checkout() {
-    window.location.href = 'checkout.html';
+document.getElementById('complexMealBtn').addEventListener('click', function () {
+    const complexMealSubMenu = document.getElementById('complexMealSubMenu');
+    if (complexMealSubMenu.style.display === 'none' || complexMealSubMenu.style.display === '') {
+        complexMealSubMenu.style.display = 'block';
+    } else {
+        complexMealSubMenu.style.display = 'none';
+    }
+});
+
+function addItemListener(id) {
+    document.getElementById(id).addEventListener('change', function () {
+        const selectedItem = this.value;
+        const cartItems = document.querySelectorAll('#cartItems li');
+
+        let alreadyExists = false;
+        cartItems.forEach(function (item) {
+            if (item.textContent.includes(selectedItem)) {
+                item.remove();
+                alreadyExists = true;
+            }
+        });
+
+        if (!alreadyExists) {
+            addToCart(selectedItem);
+        }
+    });
 }
 
-document.getElementById('firstCourse').addEventListener('change', function() {
-    const selectedFirstCourse = this.value;
-    addToCart(selectedFirstCourse);
-});
-
-document.getElementById('secondCourse').addEventListener('change', function() {
-    const selectedSecondCourse = this.value;
-    addToCart(selectedSecondCourse);
-});
-
-document.getElementById('drink').addEventListener('change', function() {
-    const selectedDrink = this.value;
-    addToCart(selectedDrink);
-});
-
-document.getElementById('salad').addEventListener('change', function() {
-    const selectedSalad = this.value;
-    addToCart(selectedSalad);
-});
+addItemListener('firstCourse');
+addItemListener('secondCourse');
+addItemListener('drink');
+addItemListener('salad');
