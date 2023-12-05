6daac2b82382f75e98d9eb465eb96a77b11932fc
vladlenn13 <146069086+vladlenn13@users.noreply.github.com>
2023-12-05 19:07:55 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,124 +1,66 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const cellContainer = document.getElementById('cellContainer');
+document.addEventListener('DOMContentLoaded', function() {
+    var complexMealBtn = document.getElementById('complexMealBtn');
+    var complexMealSubMenu = document.getElementById('complexMealSubMenu');
+    var confirmComplexMealBtn = document.getElementById('confirmComplexMealBtn');
+    var checkoutBtn = document.getElementById('checkoutBtn');
+    var cartItems = document.getElementById('cartItems');
 
-    for (let i = 1; i <= 5; i++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.dataset.id = i;
+    function updateOrderList(orderElementId, orderText) {
+        var existingOrderItem = document.getElementById(`${orderElementId}-item`);
 
-        // Создание QR-кода для каждой ячейки
-        const qr = new QRious({
-            value: `https://vladlenn13.github.io/Web/order/${i}`, // Замените ссылку на вашу страницу со статусом ячейки
-            size: 100 // Размер QR-кода
-        });
+        if (!existingOrderItem) {
+            var li = document.createElement('li');
+            li.textContent = orderText;
+            li.id = `${orderElementId}-item`;
 
-        const qrImage = document.createElement('img');
-        qrImage.src = qr.toDataURL();
-        qrImage.alt = `QR-код для ячейки ${i}`;
+            var deleteIcon = document.createElement('span');
+            deleteIcon.textContent = '❌';
+            deleteIcon.classList.add('deleteItem');
+            deleteIcon.addEventListener('click', function() {
+                li.remove();
+            });
 
-        cell.appendChild(qrImage);
-        cellContainer.appendChild(cell);
+            li.appendChild(deleteIcon);
+            cartItems.appendChild(li);
+        }
     }
 
-    const cells = document.querySelectorAll('.cell');
+    complexMealBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+        complexMealSubMenu.style.display = 'block';
+        confirmComplexMealBtn.style.display = 'block';
+    });
 
-    cells.forEach(cell => {
-        cell.addEventListener('click', () => {
-            const isBooked = cell.classList.contains('booked');
-            const isReserved = cell.classList.contains('reserved');
+    confirmComplexMealBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+        var firstCourse = document.getElementById('firstCourse').value;
+        var secondCourse = document.getElementById('secondCourse').value;
+        var drink = document.getElementById('drink').value;
+        var salad = document.getElementById('salad').value;
+        var orderText = `Комплексный обед: ${firstCourse}, ${secondCourse}, ${drink}, ${salad}`;
+        updateOrderList('complexMeal', orderText);
+        complexMealSubMenu.style.display = 'none';
+        confirmComplexMealBtn.style.display = 'none';
+        resetSelects();
+    });
 
-            if (isBooked) {
-                alert('Ячейка занята, выберете другую!');
-            } else if (isReserved) {
-                const confirmCancel = confirm('Вы хотите отменить бронь на эту ячейку?');
-                if (confirmCancel) {
-                    cell.classList.remove('reserved');
-                    cell.classList.add('booked');
-                    alert('Вы успешно отменили бронь на ячейке');
-                }
-            } else {
-                const confirmBooking = confirm(`Вы хотите забронировать ячейку ${cell.dataset.id}?`);
-                if (confirmBooking) {
-                    cell.classList.remove('cell');
-                    cell.classList.add('reserved');
-                    alert(`Вы успешно забронировали ячейку ${cell.dataset.id}`);
-                }
-            }
+    function resetSelects() {
+        document.getElementById('firstCourse').selectedIndex = 0;
+        document.getElementById('secondCourse').selectedIndex = 0;
+        document.getElementById('drink').selectedIndex = 0;
+        document.getElementById('salad').selectedIndex = 0;
+    }
+
+    checkoutBtn.addEventListener('click', function() {
+        // Дополнительная логика для оформления заказа, если необходимо
+        // Здесь вы можете добавить обработку оформления заказа
+    });
+
+    var menuItems = document.querySelectorAll('.menuItem');
+    menuItems.forEach(function(item) {
+        item.addEventListener('click', function() {
+            var itemName = this.textContent;
+            updateOrderList(itemName.replace(/\s+/g, ''), itemName);
         });
     });
 });
-menu.html
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <title>Меню столовой Шабла</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-
-<body>
-    <header>
-        <h1>Меню столовой Шабла</h1>
-        <nav>
-            <ul class="menu">
-                <li class="dropdown">
-                    <a href="#" id="complexMealBtn">Комплексный обед</a>
-                    <ul class="submenu" id="complexMealSubMenu" style="display: none;">
-                        <li>Первое блюдо:
-                            <select id="firstCourse">
-                                <option value="Суп">Суп</option>
-                                <option value="Борщ">Борщ</option>
-                                <option value="Солянка">Солянка</option>
-                            </select>
-                        </li>
-                        <li>Второе блюдо:
-                            <select id="secondCourse">
-                                <option value="Котлеты">Котлеты</option>
-                                <option value="Рыба">Рыба</option>
-                                <option value="Стейк">Стейк</option>
-                            </select>
-                        </li>
-                        <li>Напиток:
-                            <select id="drink">
-                                <option value="Чай">Чай</option>
-                                <option value="Кофе">Кофе</option>
-                                <option value="Сок">Сок</option>
-                            </select>
-                        </li>
-                        <li>Салаты:
-                            <select id="salad">
-                                <option value="Цезарь">Цезарь</option>
-                                <option value="Греческий">Греческий</option>
-                                <option value="Оливье">Оливье</option>
-                            </select>
-                        </li>
-                        <li><button id="confirmComplexMealBtn" style="display: none;">Подтвердить</button></li>
-                    </ul>
-                </li>
-                <li><a href="#" class="menuItem">Пицца</a></li>
-                <li><a href="#" class="menuItem">Суши</a></li>
-                <li><a href="#" class="menuItem">Липтон</a></li>
-                <li><a href="#" class="menuItem">Колбаса</a></li>
-            </ul>
-        </nav>
-    </header>
-
-    <main>
-        <div class="cart">
-            <h2>Корзина</h2>
-            <ul id="cartItems">
-                <!-- Сюда будут добавляться выбранные товары -->
-            </ul>
-            <button id="checkoutBtn">Оформить заказ</button>
-        </div>
-    </main>
-
-    <footer>
-        <!-- Здесь можете добавить дополнительную информацию или ссылки -->
-    </footer>
-
-    <script src="app.js"></script>
-</body>
-
-</html>
