aa4f09e447f18c02ea368c66366a53a415379e98
vladlenn13 <146069086+vladlenn13@users.noreply.github.com>
2023-12-05 08:07:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,56 +1,52 @@
-// Определяем обработчик для открытия/закрытия подменю
-function handleSubMenu(submenuElement) {
-  submenuElement.addEventListener('click', function(e) {
-    e.preventDefault();
-    var submenu = this.nextElementSibling;
-    submenu.style.display = submenu.style.display === 'none' || submenu.style.display === '' ? 'block' : 'none';
-  });
-}
-
-// Определяем обработчик для выбора пункта меню
-function handleOrderSelection(orderElementId, isSubMenu) {
-  var orderItem = document.getElementById(orderElementId);
-  orderItem.addEventListener('click', function() {
-    if (!isSubMenu) {
-      this.classList.toggle('checked');
-      updateOrderList(this.id, this.classList.contains('checked'), this.textContent);
-    }
-  });
-}
-
-// Функция обновления списка заказа
-function updateOrderList(orderElementId, isChecked, orderText) {
-  var orderList = document.getElementById('cartItems');
-  var existingOrderItem = document.querySelector(`#${orderElementId}-item`);
-
-  if (isChecked && !existingOrderItem) {
-    var li = document.createElement('li');
-    li.textContent = orderText;
-    li.id = `${orderElementId}-item`;
-
-    // Добавляем крестик для удаления из корзины
-    var deleteIcon = document.createElement('span');
-    deleteIcon.textContent = '❌';
-    deleteIcon.classList.add('deleteItem');
-    deleteIcon.addEventListener('click', function () {
-      li.remove();
+document.addEventListener('DOMContentLoaded', function() {
+    var complexMealBtn = document.getElementById('complexMealBtn');
+    var complexMealSubMenu = document.getElementById('complexMealSubMenu');
+    complexMealBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+        complexMealSubMenu.style.display = complexMealSubMenu.style.display === 'none' || complexMealSubMenu.style.display === '' ? 'block' : 'none';
     });
 
-    li.appendChild(deleteIcon);
-    orderList.appendChild(li);
-  } else if (!isChecked && existingOrderItem) {
-    existingOrderItem.remove();
-  }
-}
+    function updateOrderList(orderElementId, isChecked, orderText) {
+        var orderList = document.getElementById('cartItems');
+        var existingOrderItem = document.querySelector(`#${orderElementId}-item`);
 
-document.addEventListener('DOMContentLoaded', function() {
+        if (isChecked && !existingOrderItem) {
+            var li = document.createElement('li');
+            li.textContent = orderText;
+            li.id = `${orderElementId}-item`;
+
+            var deleteIcon = document.createElement('span');
+            deleteIcon.textContent = '❌';
+            deleteIcon.classList.add('deleteItem');
+            deleteIcon.addEventListener('click', function() {
+                li.remove();
+            });
+
+            li.appendChild(deleteIcon);
+            orderList.appendChild(li);
+        } else if (!isChecked && existingOrderItem) {
+            existingOrderItem.remove();
+        }
+    }
+
     var menuItems = document.querySelectorAll('.menu ul li a');
     menuItems.forEach(function(item) {
         if (item.nextElementSibling && item.nextElementSibling.tagName === 'UL') {
-            handleSubMenu(item);
-            handleOrderSelection(item.id, true); // Добавляем обработчик для пунктов меню-подменю
+            item.addEventListener('click', function(e) {
+                e.preventDefault();
+                var submenu = this.nextElementSibling;
+                submenu.style.display = submenu.style.display === 'none' || submenu.style.display === '' ? 'block' : 'none';
+            });
         } else {
-            handleOrderSelection(item.id, false); // Добавляем обработчик для пунктов меню-заказа
+            item.addEventListener('click', function() {
+                this.classList.toggle('checked');
+                updateOrderList(this.id, this.classList.contains('checked'), this.textContent);
+            });
         }
+    });
+
+    var checkoutBtn = document.getElementById('checkoutBtn');
+    checkoutBtn.addEventListener('click', function() {
+        // Добавьте здесь логику оформления заказа, если требуется
     });
 });
