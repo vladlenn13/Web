88b34709e69c4c53138abf0725aa3f74b68202fc
vladlenn13 <146069086+vladlenn13@users.noreply.github.com>
2023-12-05 20:08:53 +0300

    Update order.js

--- order.js
+++ order.js
@@ -1,79 +1,52 @@
 document.addEventListener('DOMContentLoaded', function () {
     const cellContainer = document.getElementById('cellContainer');
-    const timeSlots = generateTimeSlots(); // Генерация временных слотов
 
     for (let i = 1; i <= 5; i++) {
         const cellWrapper = document.createElement('div');
         cellWrapper.classList.add('cell');
 
         const cellNumber = document.createElement('p');
-        cellNumber.textContent = `Ячейка №${i}`;
+        cellNumber.textContent = Ячейка №${i};
         cellNumber.style.marginBottom = '5px';
 
+        const qrContainer = document.createElement('div');
+        qrContainer.classList.add('qr-container');
+
         const qr = new QRious({
-            value: `https://vladlenn13.github.io/Web/order/${i}`,
+            value: https://vladlenn13.github.io/Web/order/${i},
             size: 100
         });
 
         const qrImage = document.createElement('img');
         qrImage.src = qr.toDataURL();
-        qrImage.alt = `QR-код для ячейки ${i}`;
+        qrImage.alt = QR-код для ячейки ${i};
 
+        qrContainer.appendChild(qrImage);
         cellWrapper.appendChild(cellNumber);
-        cellWrapper.appendChild(qrImage);
+        cellWrapper.appendChild(qrContainer);
         cellContainer.appendChild(cellWrapper);
-
-        if (localStorage.getItem(`cell_${i}`) === 'reserved') {
-            cellWrapper.classList.add('reserved');
-            const reservationInfo = JSON.parse(localStorage.getItem(`reservation_${i}`));
-            const reservationText = document.createElement('p');
-            reservationText.textContent = `Ячейка забронирована пользователем: ${reservationInfo.name} Время бронирования: ${reservationInfo.time}`;
-            cellWrapper.appendChild(reservationText);
-        }
 
         cellWrapper.addEventListener('click', () => {
             const isBooked = cellWrapper.classList.contains('booked');
             const isReserved = cellWrapper.classList.contains('reserved');
 
             if (isBooked) {
-                alert('Ячейка занята, выберите другую!');
+                alert('Ячейка занята, выберете другую!');
             } else if (isReserved) {
-                const reservationInfo = JSON.parse(localStorage.getItem(`reservation_${i}`));
-                alert(`Ячейка забронирована пользователем: ${reservationInfo.name} Время бронирования: ${reservationInfo.time}`);
+                const confirmCancel = confirm('Вы хотите отменить бронь на эту ячейку?');
+                if (confirmCancel) {
+                    cellWrapper.classList.remove('reserved');
+                    cellWrapper.classList.add('booked');
+                    alert('Вы успешно отменили бронь на ячейке');
+                }
             } else {
-                const name = prompt('Введите ваше имя:');
-                if (name) {
-                    const selectedTime = prompt('Выберите время бронирования:\n' + timeSlots.join('\n'));
-                    if (selectedTime && timeSlots.includes(selectedTime)) {
-                        const time = new Date().toLocaleString();
-                        const reservationInfo = { name, time, selectedTime };
-                        localStorage.setItem(`cell_${i}`, 'reserved');
-                        localStorage.setItem(`reservation_${i}`, JSON.stringify(reservationInfo));
-                        cellWrapper.classList.add('reserved');
-                        const reservationText = document.createElement('p');
-                        reservationText.textContent = `Ячейка забронирована пользователем: ${name} Время бронирования: ${time}, ${selectedTime}`;
-                        cellWrapper.appendChild(reservationText);
-                        alert(`Вы успешно забронировали ячейку ${i}`);
-                    } else {
-                        alert('Выберите корректное время из списка');
-                    }
+                const confirmBooking = confirm(Вы хотите забронировать ячейку ${i}?);
+                if (confirmBooking) {
+                    cellWrapper.classList.remove('cell');
+                    cellWrapper.classList.add('reserved');
+                    alert(Вы успешно забронировали ячейку ${i});
                 }
             }
         });
     }
 });
-
-function generateTimeSlots() {
-    const timeSlots = [];
-    const startTime = 10 * 60; // Время в минутах (10:00)
-    const endTime = 17 * 60; // Время в минутах (17:00)
-    const interval = 20; // Интервал в минутах
-
-    for (let minute = startTime; minute < endTime; minute += interval) {
-        const hours = Math.floor(minute / 60).toString().padStart(2, '0');
-        const minutes = (minute % 60).toString().padStart(2, '0');
-        timeSlots.push(`${hours}:${minutes}`);
-    }
-
-    return timeSlots;
-}
