2ac3296c505b90bae6da034f0ae313716aac7f96
vladlenn13 <146069086+vladlenn13@users.noreply.github.com>
2023-12-05 07:51:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,55 @@
-function addToCart(item) {
-    const cartItems = document.getElementById('cartItems');
-    const listItem = document.createElement('li');
-    listItem.textContent = item;
+// Определяем обработчик для открытия/закрытия подменю
+function handleSubMenu(submenuElement) {
+  submenuElement.addEventListener('click', function(e) {
+    e.preventDefault();
+    var submenu = this.nextElementSibling;
+    submenu.style.display = submenu.style.display === 'none' || submenu.style.display === '' ? 'block' : 'none';
+  });
+}
 
-    // Добавление крестика для удаления из корзины
-    const deleteIcon = document.createElement('span');
+// Определяем обработчик для выбора пункта меню
+function handleOrderSelection(orderElementId, isSubMenu) {
+  var orderItem = document.getElementById(orderElementId);
+  orderItem.addEventListener('click', function() {
+    if (!isSubMenu) {
+      this.classList.toggle('checked');
+      updateOrderList(this.id, this.classList.contains('checked'), this.textContent);
+    }
+  });
+}
+
+// Функция обновления списка заказа
+function updateOrderList(orderElementId, isChecked, orderText) {
+  var orderList = document.getElementById('cartItems');
+  var existingOrderItem = document.querySelector(`#${orderElementId}-item`);
+
+  if (isChecked && !existingOrderItem) {
+    var li = document.createElement('li');
+    li.textContent = orderText;
+    li.id = `${orderElementId}-item`;
+
+    // Добавляем крестик для удаления из корзины
+    var deleteIcon = document.createElement('span');
     deleteIcon.textContent = '❌';
     deleteIcon.classList.add('deleteItem');
     deleteIcon.addEventListener('click', function () {
-        listItem.remove();
+      li.remove();
     });
 
-    listItem.appendChild(deleteIcon);
-    cartItems.appendChild(listItem);
+    li.appendChild(deleteIcon);
+    orderList.appendChild(li);
+  } else if (!isChecked && existingOrderItem) {
+    existingOrderItem.remove();
+  }
 }
 
-document.getElementById('complexMealBtn').addEventListener('click', function () {
-    const complexMealSubMenu = document.getElementById('complexMealSubMenu');
-    if (complexMealSubMenu.style.display === 'none' || complexMealSubMenu.style.display === '') {
-        complexMealSubMenu.style.display = 'block';
-    } else {
-        complexMealSubMenu.style.display = 'none';
-    }
+// Назначаем обработчики для всех пунктов меню и подменю
+var menuItems = document.querySelectorAll('.menu ul li a');
+menuItems.forEach(function(item) {
+  if (item.nextElementSibling && item.nextElementSibling.tagName === 'UL') {
+    handleSubMenu(item);
+    handleOrderSelection(item.id, true); // Добавляем обработчик для пунктов меню-подменю
+  } else {
+    handleOrderSelection(item.id, false); // Добавляем обработчик для пунктов меню-заказа
+  }
 });
-
-function addItemListener(id) {
-    document.getElementById(id).addEventListener('change', function () {
-        const selectedItem = this.value;
-        const cartItems = document.querySelectorAll('#cartItems li');
-
-        let alreadyExists = false;
-        cartItems.forEach(function (item) {
-            if (item.textContent.includes(selectedItem)) {
-                item.remove();
-                alreadyExists = true;
-            }
-        });
-
-        if (!alreadyExists) {
-            addToCart(selectedItem);
-        }
-    });
-}
-
-addItemListener('firstCourse');
-addItemListener('secondCourse');
-addItemListener('drink');
-addItemListener('salad');
